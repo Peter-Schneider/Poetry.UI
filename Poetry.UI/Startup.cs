13c41ab82b70da41ec43c62be3913645225c364e
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-05-04 14:53:26 +0200

    Add convenience method AddPoetryUI

--- Poetry.UI/Startup.cs
+++ Poetry.UI/Startup.cs
@@ -11,11 +11,33 @@ using Poetry.UI.EmbeddedResourceSupport;
 using Poetry.UI.MvcSupport;
 using Poetry.UI.AppSupport;
 using System.Reflection;
+using Unity;
+using Unity.AspNet.Mvc;
 
 namespace Poetry.UI
 {
     public static class Startup
     {
+        public static void AddPoetryUI(this HttpApplication application)
+        {
+            var container = new UnityContainer();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+            
+            application.AddPoetryUI((t, i) => {
+                if (i is Type)
+                {
+                    container.RegisterSingleton(t, (Type)i);
+                }
+                else
+                {
+                    container.RegisterInstance(t, i);
+                }
+            }, new List<Assembly> {
+                Assembly.GetCallingAssembly()
+            },
+            "Admin");
+        }
+
         public static void AddPoetryUI(this HttpApplication application, Action<Type, object> registerSingletonInDependencyResolver, IEnumerable<Assembly> assemblies, string basePath = "Poetry")
         {
             RouteTable.Routes.MapRoute(
