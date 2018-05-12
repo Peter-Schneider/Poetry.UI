6123fcde8c0cead2c25540a7e7d8c57443b840fb
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-05-12 13:44:00 +0200

    Move form code into Forms component

---
+++ Poetry.UI.FormSupport/Scripts/form-elements.js
@@ -0,0 +1,35 @@
+﻿var formElements = {};
+
+formElements['string'] = {
+    type: 'string',
+    create: function (propertyDefinition, get, set) {
+        var label = document.createElement('label');
+        label.classList.add('form-label');
+
+        var labelText = document.createElement('div');
+        labelText.classList.add('form-label-text');
+        labelText.innerText = propertyDefinition.name;
+        label.appendChild(labelText);
+
+        var input = document.createElement('input');
+        input.classList.add('form-input');
+        label.appendChild(input);
+
+        input.type = 'text';
+
+        var value = get();
+
+        if (typeof value != 'undefined' && typeof value != 'null') {
+            input.value = get();
+        }
+
+        function update() {
+            set(input.value);
+        }
+
+        input.addEventListener('change', update);
+        input.addEventListener('keyup', update);
+
+        return label;
+    }
+};
---
+++ Poetry.UI.FormSupport/Scripts/property-definition-provider.js
@@ -0,0 +1,19 @@
+﻿
+
+
+/* PROPERTY DEFINITION PROVIDER */
+
+class PropertyDefinitionProvider {
+    getFor(id) {
+        return fetch(`Form/GetPropertyDefinitionsFor?id=${id}`, {
+            credentials: 'include'
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`${response.status} (${response.statusText})`);
+                }
+
+                return response.json();
+            });
+    }
+}
