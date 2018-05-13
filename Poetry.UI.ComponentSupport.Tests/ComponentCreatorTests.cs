0763affe89359ba9a5e2f93deb7208936f577d1b
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-05-13 11:26:55 +0200

    Remove test until prerequisites are implemented

--- Poetry.UI.ComponentSupport.Tests/ComponentCreatorTests.cs
+++ Poetry.UI.ComponentSupport.Tests/ComponentCreatorTests.cs
@@ -8,14 +8,5 @@ namespace Poetry.UI.ComponentSupport.Tests
 {
     public class ComponentCreatorTests
     {
-        [Fact]
-        public void Works()
-        {
-            var reflector = Mock.Of<IReflector>();
-
-            //Mock.Get(reflector).Setup(r => r.GetReflectorType(typeof(string))).Returns(new ReflectorType("string", new List<ReflectorAttribute> { new ReflectorAttribute("Component", new Dictionary<string, string> { ["Id"] = "my-component" }) }));
-
-            Assert.NotNull(new ComponentCreator(null).Create(typeof(string)));
-        }
     }
 }
