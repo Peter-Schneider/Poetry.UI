3581e26456352d65256ffed41b09ac94be51996e
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-05-19 11:08:19 +0200

    Unseal class https://github.com/poetry-dotnet/Poetry.UI/issues/21

--- Poetry.UI.ControllerSupport/ControllerAction.cs
+++ Poetry.UI.ControllerSupport/ControllerAction.cs
@@ -8,7 +8,7 @@ namespace Poetry.UI.ControllerSupport
     /// <summary>
     /// Represents a Poetry.UI controller action. Type is prefixed with Controller as to avoid collision with System.Action.
     /// </summary>
-    public sealed class ControllerAction
+    public class ControllerAction
     {
         public string Id { get; }
         public MethodInfo Method { get; }
