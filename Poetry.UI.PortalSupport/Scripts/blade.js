e5fb1ba98ed0f9b81929d26e66585f543ad18b9f
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-05-26 14:09:18 +0200

    Remove event system, replace with onClose
    Fixes https://github.com/poetry-dotnet/Poetry.UI/issues/41

--- Poetry.UI.PortalSupport/Scripts/blade.js
+++ Poetry.UI.PortalSupport/Scripts/blade.js
@@ -7,30 +7,21 @@ class Blade {
     constructor() {
         this.name = this.constructor.name;
         this.root = document.createElement('blade');
-        this.events = {};
     }
 
     open(data) {
 
     }
 
+    onClose(callback) {
+        this.onCloseCallback = callback;
+
+        return this;
+    }
+
     close(data) {
-        this.trigger('close', data);
-    }
-
-    addEventListener(event, callback) {
-        if (!this.events[event]) {
-            this.events[event] = [];
+        if (this.onCloseCallback) {
+            this.onCloseCallback(data);
         }
-
-        this.events[event].push(callback);
-    }
-
-    trigger(event, data) {
-        if (!this.events[event]) {
-            return;
-        }
-
-        this.events[event].forEach(callback => callback(data));
     }
 }
