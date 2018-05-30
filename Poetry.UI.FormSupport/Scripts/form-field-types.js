1bc61c22ea573677412f367ccafc7d39c4594be0
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-05-30 11:59:45 +0200

    Add field type 'double'

--- Poetry.UI.FormSupport/Scripts/form-field-types.js
+++ Poetry.UI.FormSupport/Scripts/form-field-types.js
@@ -33,3 +33,38 @@ formFieldTypes['string'] = {
         return label;
     }
 };
+
+formFieldTypes['double'] = {
+    type: 'double',
+    createControl: function (field, get, set) {
+        var label = document.createElement('label');
+        label.classList.add('form-label');
+
+        var labelText = document.createElement('div');
+        labelText.classList.add('form-label-text');
+        labelText.innerText = field.Name;
+        label.appendChild(labelText);
+
+        var input = document.createElement('input');
+        input.classList.add('form-input');
+        label.appendChild(input);
+
+        input.type = 'number';
+        input.step = 'any';
+
+        var value = get();
+
+        if (typeof value != 'undefined' && typeof value != 'null') {
+            input.value = get();
+        }
+
+        function update() {
+            set(input.value);
+        }
+
+        input.addEventListener('change', update);
+        input.addEventListener('keyup', update);
+
+        return label;
+    }
+};
