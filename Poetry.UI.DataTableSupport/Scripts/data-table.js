9dea172a2d70c0e023797a9b931217e22e1bd68c
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-06-16 17:30:52 +0200

    Implement copy to excel, fixes https://github.com/poetry-dotnet/Poetry.UI/issues/31

--- Poetry.UI.DataTableSupport/Scripts/data-table.js
+++ Poetry.UI.DataTableSupport/Scripts/data-table.js
@@ -209,3 +209,84 @@ class DataTableColumn {
         return this;
     }
 }
+
+
+
+/* COPY AS TAB SEPARATED */
+
+class DataTableCopyAsTabSeparated {
+    constructor(dataTable) {
+        this.dataTable = dataTable;
+    }
+
+    copy() {
+        return this.getPages()
+            .then(pages => {
+                var result = this.getHeaders();
+
+                pages.forEach(page => page.Items.forEach(item => result += this.getRow(item)));
+
+                return navigator.clipboard.writeText(result);
+            });
+    }
+
+    getPages() {
+        return this.dataTable.backend
+            .load({
+                page: 1,
+                sortBy: this.dataTable.sortBy,
+                sortDirection: this.dataTable.sortDirection,
+            })
+            .then(page => [page]);
+    }
+
+    getHeaders() {
+        var result = [];
+
+        this.dataTable.columns
+            .forEach(column => {
+                if (column.actionColumn) {
+                    return;
+                }
+
+                if (column.headerGenerator) {
+                    var content = column.headerGenerator(this.dataTable);
+
+                    if (content instanceof Node) {
+                        result.push(content.innerText);
+                    } else if (content.element instanceof Node) {
+                        result.push(content.element.innerText);
+                    } else {
+                        result.push(content);
+                    }
+                }
+            });
+
+        return result.join('\t') + '\n';
+    }
+
+    getRow(item) {
+        var result = [];
+
+        this.dataTable.columns
+            .forEach(column => {
+                if (column.actionColumn) {
+                    return;
+                }
+
+                if (column.contentGenerator) {
+                    var content = column.contentGenerator(item, this.dataTable);
+
+                    if (content instanceof Node) {
+                        result.push(content.innerText);
+                    } else if (content.element instanceof Node) {
+                        result.push(content.element.innerText);
+                    } else {
+                        result.push(content);
+                    }
+                }
+            });
+
+        return result.join('\t') + '\n';
+    }
+}
