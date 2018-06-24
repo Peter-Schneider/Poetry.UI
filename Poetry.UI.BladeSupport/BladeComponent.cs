f0cb4e1f821ad217317de291662f8d52f54a70a1
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2018-06-24 10:27:58 +0200

    Move blade script imports to BladeSupport, closes https://github.com/poetry-dotnet/Poetry.UI/issues/262

--- Poetry.UI.BladeSupport/BladeComponent.cs
+++ Poetry.UI.BladeSupport/BladeComponent.cs
@@ -1,9 +1,12 @@
 ﻿using Poetry.UI.ComponentSupport;
+using Poetry.UI.ScriptSupport;
 using System;
 
 namespace Poetry.UI.BladeSupport
 {
     [Component("Poetry.UI.BladeSupport")]
+    [Script("Scripts/blade.js")]
+    [Script("Scripts/close-button.js")]
     public class BladeComponent
     {
     }
